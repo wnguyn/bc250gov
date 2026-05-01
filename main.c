9f8a7279ad70b462c5e4549474c45c9fbbe8334e
bitsixfour <tugirasles@gmail.com>
2026-05-01 13:20:23 -0500

    boilerplate

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <libdrm/amdgpu.h>
+#include <libdrm/amdgpu_drm.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdint.h>
+#include "toml.h"
+
+int main() {
+    println("test");
+}

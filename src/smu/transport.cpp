b6c3551a4129c374007f9c9ac20f5be057ea607a
bob <tugirasles@gmail.com>
2026-06-26 17:42:59 -0500

    askjdhds

---
+++ src/smu/transport.cpp
@@ -0,0 +1,44 @@
+#include "transport.hpp"
+
+#include <unistd.h>
+#include <string>
+#include <fstream>
+#include <cstdint>
+#include <iostream>
+#include <filesystem>
+#include <bit>
+
+
+CyanTransport::CyanTransport(std::string std) {
+  std::filesystem::path config_path = std::filesystem::path("/sys/bus/pci/devices/") / std / "config";
+  this->bus_path = config_path;
+  this->flock = true;
+  this->file = std::fstream(this->bus_path);
+}
+
+CyanTransport::~CyanTransport() {
+  this->file.close();
+}
+
+uint32_t CyanTransport::read_config32(uint32_t ofst) {
+  uint8_t buf[4] = {0};
+  this->file.seekg(ofst, std::ios::beg);
+  this->file.read(reinterpret_cast<char*>(buf), 4);
+  // turn into little endian manually
+  uint32_t value = (static_cast<uint32_t>(buf[0]) << 0)  |
+                 (static_cast<uint32_t>(buf[1]) << 8)  |
+                 (static_cast<uint32_t>(buf[2]) << 16) |
+                 (static_cast<uint32_t>(buf[3]) << 24);
+  return value;
+}
+
+void CyanTransport::write_config32(uint64_t offset, uint32_t val) {
+    uint8_t buf[4] = {
+        (uint8_t)(val >> 24),
+        (uint8_t)(val >> 16),
+        (uint8_t)(val >> 8),
+        (uint8_t)(val)
+    };
+    this->file.seekp(offset, std::ios::beg);
+    this->file.write(reinterpret_cast<char*>(buf), 4);
+}
--- src/smu/transport.hpp
+++ src/smu/transport.hpp
@@ -1,24 +1,21 @@
 #include <cstdint>
-#include <iostream>
+#include <fstream>
 #include <filesystem>
-
-
 /* Send binary offsets to doohickey */
 
 class CyanTransport {
   public: 
-    CyanTransport();
+    CyanTransport(std::string std);
     ~CyanTransport();
-    bool open();
-    void close();
-    bool read_config32(uint32_t offset);
-    bool write_config32(uint64_t offset, uint32_t val);
+    uint32_t read_config32(uint32_t offset);
+    void write_config32(uint64_t offset, uint32_t val);
     uint32_t read_smu(uint32_t reg);
-    uint32_t write_smu(uint32_t reg);
+    uint32_t write_smu(uint32_t offset, uint32_t val);
 
 
 
   private:
     std::filesystem::path bus_path;
-    int file;
+    std::fstream file;
+    bool flock;
 };
